349996ee3c0cac4ffae549c5b3c0227251637ffe
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-11-08 20:36:30 -0500

    feat: main python functions

---
+++ internal.sh
@@ -0,0 +1,19 @@
+curl 'https://telefonica-pe.etadirect.com/mobility/?m=sync&a=write&ajax=1&window_id=1604882883-0000-0000' \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'X-PLATFORM: 1' \
+  -H 'X-OA: 2' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36' \
+  -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryQztSKdSTCAtACZZ3' \
+  -H 'Origin: https://telefonica-pe.etadirect.com' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Referer: https://telefonica-pe.etadirect.com/mobility/' \
+  -H 'Accept-Language: en,es;q=0.9,es-ES;q=0.8,la;q=0.7,fr;q=0.6,pt;q=0.5' \
+  -H 'Cookie: disableAppCache=1; build=20.5.13.0.14; lastAuthId=toa_._telefonica-pe_.__._mobility; ofsc42=NDljOGFjN2Q2OGM0YjU0NzczYWY1ZTIzYzQ1ZmQwYmUwMDAtYTEwMzJhMWRmYjcyMzhlOWMwM2RjYjAwM2ViNDdiMmItZjY0Njc3ZTlhZDk4Mzc0MzhhMWYzNWJiMWI2Njc2YzU%3D' \
+  --data-binary $'------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="__protocol"\r\n\r\n5\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="dv"\r\n\r\n{"q":"28140|20201107162858"}\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="pid"\r\n\r\n5212\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="u"\r\n\r\nHACKATON14\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="f"\r\n\r\njson\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="pids"\r\n\r\n[]\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="aids"\r\n\r\n["9129632","9179394","9203927","9226710"]\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="restriction"\r\n\r\n0\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="qid"\r\n\r\n1865256\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="fakeIds"\r\n\r\n{}\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="trust"\r\n\r\n$fast$sha256$e9d077e83dc399b8580af967b558d966c96807e74718e0dd5652c05d975074c7.76f584af4f25fb2123f9ad1ecdc50ac0\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="fakeIdsClean"\r\n\r\n0\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="dq"\r\n\r\n2020-10-09\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="date"\r\n\r\n2020-10-09\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="dispatcher"\r\n\r\n1\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="mandatory[1][0]"\r\n\r\n9203927\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="limitActivitiesByPool[notscheduled]"\r\n\r\n5\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3\r\nContent-Disposition: form-data; name="skip_delta"\r\n\r\n0\r\n------WebKitFormBoundaryQztSKdSTCAtACZZ3--\r\n' \
+  --compressed
